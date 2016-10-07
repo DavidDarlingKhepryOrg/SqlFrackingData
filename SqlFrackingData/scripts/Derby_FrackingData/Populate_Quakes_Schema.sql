55bd5977c7ce01d90f9e46117d5df34df656d90c
Khepry Quixote <Khepry Quixote khepry.quixote@gmail.com>
2016-10-07 18:02:21 -0500

    Rename DateTime to Event_DTG throughout the DDL scripts.

--- SqlFrackingData/scripts/Derby_FrackingData/Populate_Quakes_Schema.sql
+++ SqlFrackingData/scripts/Derby_FrackingData/Populate_Quakes_Schema.sql
@@ -10,7 +10,7 @@ DROP VIEW QUAKES.vue_Quakes_Oklahoma_vs_California;
 DROP TABLE QUAKES.Quakes;
 
 CREATE TABLE QUAKES.Quakes (
-	DateTime TIMESTAMP,
+	Event_DTG VARCHAR (25),
 	Latitude DOUBLE,
 	Longitude DOUBLE,
 	Depth REAL,
@@ -25,6 +25,9 @@ CREATE TABLE QUAKES.Quakes (
 	Event_Year VARCHAR(4),
 	Event_Month VARCHAR(2),
 	Event_Day VARCHAR(2),
+	Event_Hour VARCHAR(2),
+	Event_Min VARCHAR(2),
+	Event_Sec VARCHAR(2),
 	cc VARCHAR (10),
 	admin1 VARCHAR (100),
 	admin2 VARCHAR (100),
@@ -37,7 +40,7 @@ CREATE INDEX QUAKES.Quakes_cc ON QUAKES.Quakes (cc);
 CREATE INDEX QUAKES.Quakes_admin1 ON QUAKES.Quakes (admin1);
 CREATE INDEX QUAKES.Quakes_admin2 ON QUAKES.Quakes (admin2);
 CREATE INDEX QUAKES.Quakes_name ON QUAKES.Quakes (name);
-CREATE INDEX QUAKES.Quakes_DateTime ON QUAKES.Quakes (DateTime);
+CREATE INDEX QUAKES.Quakes_Event_DTG ON QUAKES.Quakes (Event_DTG);
 CREATE INDEX QUAKES.Quakes_Depth ON QUAKES.Quakes (Depth);
 CREATE INDEX QUAKES.Quakes_Magnitude ON QUAKES.Quakes (Magnitude);
 CREATE INDEX QUAKES.Quakes_Latitude ON QUAKES.Quakes (Latitude);
@@ -46,7 +49,7 @@ CREATE INDEX QUAKES.Quakes_Longitude ON QUAKES.Quakes (Longitude);
 CREATE VIEW QUAKES.vue_Quakes_Magnitudes
 AS
 SELECT
-	DateTime,
+	Event_DTG,
 	Latitude,
 	Longitude,
 	Depth,
@@ -62,9 +65,12 @@ SELECT
 	admin1,
 	admin2,
 	name,
-	YEAR(DateTime) Event_Year,
-	MONTH(DateTime) Event_Month,
-	DAY(DateTime) Event_Day,
+	Event_Year,
+	Event_Month,
+	Event_Day,
+	Event_Hour,
+	Event_Min,
+	Event_Sec,
 	CASE WHEN CAST(Magnitude AS int) = 0 THEN 1 ELSE 0 END Mag_0,
 	CASE WHEN CAST(Magnitude AS int) = 1 THEN 1 ELSE 0 END Mag_1,
 	CASE WHEN CAST(Magnitude AS int) = 2 THEN 1 ELSE 0 END Mag_2,
@@ -120,7 +126,7 @@ SELECT
     Mag_Avg,
     Total,
     Total - Mag_0 Total_wo_Mag_0,
-    cast((Mag_1 + (Mag_2 * 2) + (Mag_3 * 3) + (Mag_4 * 4) + (Mag_5 * 5) + (Mag_6 * 6) + (Mag_7 * 7) + (Mag_8 * 8) + (Mag_9 * 9)) AS REAL) / cast((Total - Mag_0) AS REAL) Weighted_Avg_Mag,
+    CAST((Mag_1 + (Mag_2 * 2) + (Mag_3 * 3) + (Mag_4 * 4) + (Mag_5 * 5) + (Mag_6 * 6) + (Mag_7 * 7) + (Mag_8 * 8) + (Mag_9 * 9)) AS REAL) / CAST((Total - Mag_0) AS REAL) Weighted_Avg_Mag,
     Mag_3 + Mag_4 + Mag_5 + Mag_6 + Mag_7 + Mag_8 + Mag_8 Total_Mag_3_and_Above
 FROM
 (
@@ -128,19 +134,19 @@ FROM
         cc,
         Event_Year,
         admin1,
-        sum(Mag_0) Mag_0,
-        sum(Mag_1) Mag_1,
-        sum(Mag_2) Mag_2,
-        sum(Mag_3) Mag_3,
-        sum(Mag_4) Mag_4,
-        sum(Mag_5) Mag_5,
-        sum(Mag_6) Mag_6,
-        sum(Mag_7) Mag_7,
-        sum(Mag_8) Mag_8,
-        sum(Mag_9) Mag_9,
-        avg(Magnitude) Mag_Avg,
-        count(*) Total,
-        count(*) - sum(Mag_0) Total_wo_Mag_0
+        SUM(Mag_0) Mag_0,
+        SUM(Mag_1) Mag_1,
+        SUM(Mag_2) Mag_2,
+        SUM(Mag_3) Mag_3,
+        SUM(Mag_4) Mag_4,
+        SUM(Mag_5) Mag_5,
+        SUM(Mag_6) Mag_6,
+        SUM(Mag_7) Mag_7,
+        SUM(Mag_8) Mag_8,
+        SUM(Mag_9) Mag_9,
+        AVG(Magnitude) Mag_Avg,
+        COUNT(*) Total,
+        COUNT(*) - SUM(Mag_0) Total_wo_Mag_0
     FROM
     (
         
@@ -174,4 +180,4 @@ ORDER BY
     Event_Year DESC,
     admin1;
     
--- CALL SYSCS_UTIL.SYSCS_IMPORT_TABLE ('QUAKES','QUAKES','E:\home\data\quake-info\NCEDC_earthquakes_reverse_geocoded.csv',null,null,null,0);
+-- CALL SYSCS_UTIL.SYSCS_IMPORT_TABLE ('QUAKES','QUAKES','E:\home\data\quake-info\NCEDC_earthquakes_reverse_geocoded_test_n_header.csv',null,null,null,0);
